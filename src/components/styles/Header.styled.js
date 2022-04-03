ad691c1140bb4b1d146122d8c75bef1fff532ff0
ChoyaSpadin <choya.spadin@gmail.com>
2022-04-02 23:10:00 -0300

    header image and @media

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.colors.header};
@@ -10,8 +10,21 @@ export const Nav = styled.nav`
   align-items: center;
   justify-content: space-between;
   margin-bottom: 40px;
-  `
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    flex-direction: column;
+  }
+`
 
 export const Logo = styled.img`
- 
-  `
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    margin-bottom: 40px;
+  }
+`
+
+export const Image = styled.img`
+  width: 375px;
+  margin-left: 40px;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    margin: 40px 0 30px;
+  }
+`
